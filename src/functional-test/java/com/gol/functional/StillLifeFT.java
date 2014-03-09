0338104056a2e26b5248ba37573f64394aa9ec0f
codehackerr <code.rr@mail.com>
2014-03-09 16:46:23 +0530

    Functional test: StillLife.Beehive

--- src/functional-test/java/com/gol/functional/StillLifeFT.java
+++ src/functional-test/java/com/gol/functional/StillLifeFT.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertThat;
 
 public class StillLifeFT {
     private static final String NEW_LINE = System.getProperty("line.separator");
+
     private static final String BLOCK =
             "----" + NEW_LINE +
             "-xx-" + NEW_LINE +
@@ -19,5 +20,19 @@ public class StillLifeFT {
         Grid grid = Grid.from_string(BLOCK);
         Grid next_generation = grid.next_generation();
         assertThat(next_generation.toString(),is(BLOCK));
+    }
+
+    private static final String BEEHIVE =
+            "-----" + NEW_LINE +
+            "--xx--" + NEW_LINE +
+            "-x--x-" + NEW_LINE +
+            "--xx--" + NEW_LINE +
+            "-----" ;
+
+    @Test
+    public void beehive() {
+        Grid grid = Grid.from_string(BEEHIVE);
+        Grid next_generation = grid.next_generation();
+        assertThat(next_generation.toString(),is(BEEHIVE));
     }
 }
